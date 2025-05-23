3481e4fbf56ef9373200825f93ff90a0446f755b
kim <conmeokhung1230@gmail.com>
2025-05-23 10:12:56 +0700

    flask

--- app.py
+++ app.py
@@ -1,24 +1,18 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from models import db, User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret123'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
 admin = Admin(app, name='Quản trị', template_mode='bootstrap3')
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    email = db.Column(db.String(50))
-
 admin.add_view(ModelView(User, db.session))
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-
